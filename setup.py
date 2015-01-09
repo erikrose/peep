b329f017d78756489992a8ce9338f4fa2e6a77f1
Erik Rose <erik@mozilla.com>
2015-01-08 23:50:01 -0500

    Add Python 3 point releases to trove classifiers.

--- setup.py
+++ setup.py
@@ -37,6 +37,10 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Build Tools',
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Systems Administration'
