e0af1546312197377d3fa556e22a34969a6a750a
Erik Rose <grinch@grinchcentral.com>
2015-03-18 11:49:23 -0400

    Copy the operative portion of the MIT license into peep.py so embedding it doesn't break the license.

--- peep.py
+++ peep.py
@@ -9,6 +9,18 @@ local PyPI mirror or use a vendor lib. Just update the version numbers and
 hashes in requirements.txt, and you're all set.
 
 """
+# This is here so embedded copies of peep.py are MIT-compliant:
+# Copyright (c) 2013 Erik Rose
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 from __future__ import print_function
 try:
     xrange = xrange
