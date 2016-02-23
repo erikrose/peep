daa9e87014de1f907fac446f89b9d849665a649e
Ed Morley <emorley@mozilla.com>
2016-02-23 14:24:13 +0000

    Travis: Test peep on Python 3.5
    And add the appropriate PyPI classifier.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Build Tools',
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Systems Administration'
